57afd2af32f5ee2c25cef95ab3c21a2af2fec362
yulim <yulim13sky@naver.com>
2023-08-24 15:25:09 +0900

    feat: 로그인 페이지 controller 연결

---
+++ springboard/src/main/java/kr/ac/kopo/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+    @GetMapping("login")
+    public String loginForm() {
+        return "member/loginForm";
+        // member/loginForm.jsp
+    }
+}
--- springboard/src/main/webapp/index.jsp
+++ springboard/src/main/webapp/index.jsp
@@ -10,5 +10,6 @@
 	환영합니다. 첫 페이지에요.
 	<a href="${pageContext.request.contextPath}/board">게시판</a>
 	<a href="${pageContext.request.contextPath}/board/new">글쓰기</a>
+	<a href="${pageContext.request.contextPath}/login">로그인</a>
 </body>
 </html>
