bee4db010db6594eb340d3ba34d89fdb861ca273
yulim <yulim13sky@naver.com>
2023-08-25 11:36:45 +0900

    feat: 로그인 안하면 redirect

--- springboard/src/main/java/kr/ac/kopo/interceptor/LoginInterceptor.java
+++ springboard/src/main/java/kr/ac/kopo/interceptor/LoginInterceptor.java
@@ -2,9 +2,11 @@ package kr.ac.kopo.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import kr.ac.kopo.member.vo.MemberVO;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
@@ -19,10 +21,22 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
             Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        MemberVO memberVO = (MemberVO) session.getAttribute("currentUser");
         System.out.println("preHandle - 로그인 체크 동작입니다.");
+
+        if (memberVO == null) { // 로그인 안했으면
+            System.out.println("memberVO is null");
+            // http://localhost:8080/springboard/login
+            response.sendRedirect(request.getContextPath() + "/login");
+            return false;
+        } else { // 로그인 이미 한 경우
+            System.out.println("memberVO 로그인 했음");
+            return true;
+        }
+
         // 로그인 안했으면, 로그인 하세요
         // login.jsp 화면으로 이동
-        return true;
     }
 
 }
