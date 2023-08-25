ac52a8538914e3765bb2055636a11f372cb3335b
yulim <yulim13sky@naver.com>
2023-08-25 10:15:29 +0900

    feat: 0825 수업 로그인/로그아웃 세션

--- springboard/src/main/java/kr/ac/kopo/member/controller/MemberController.java
+++ springboard/src/main/java/kr/ac/kopo/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package kr.ac.kopo.member.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,15 +9,36 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import kr.ac.kopo.member.service.MemberService;
 import kr.ac.kopo.member.vo.LoginVO;
 import kr.ac.kopo.member.vo.MemberVO;
 
+@SessionAttributes("currentUser")
 @Controller
 public class MemberController {
 
     @Autowired
     private MemberService memberService;
+
+
+    @GetMapping("/logout")
+    public String logout2(SessionStatus sessionStatus) {
+        // session.removeAttribute("currentUser");
+        sessionStatus.setComplete();
+        return "redirect:/";
+    }
+
+    //@GetMapping("/logout")
+    public String logout(HttpSession session) {
+        // session.removeAttribute("currentUser");
+        session.invalidate();
+        // "index" WEB-INF/jsp/index.jsp (x)
+        // webapp/index.jsp
+        // http://localhost:8080/springboard/
+        return "redirect:/";
+    }
 
     @GetMapping("/login")
     public String loginForm(Model model) {
@@ -26,9 +49,10 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public String loginProcess(@Valid LoginVO loginVO, Errors errors, Model model) {
+    public String loginProcess(@Valid LoginVO loginVO, Errors errors, Model model,
+            HttpServletRequest request, HttpSession session) {
 
-        if (errors.hasErrors()) {// error 존개
+        if (errors.hasErrors()) {// error 존재, null값이 존재, 다시 쓰세요
             return "member/loginForm";
         } else { // null값이 없을 때 DB query 실행
             MemberVO memberVO = memberService.login(loginVO);
@@ -40,11 +64,12 @@ public class MemberController {
             } else {
                 System.out.println("login이 완료되었습니다.");
                 System.out.println(memberVO);
-                return "redirect:/board";
+                // session 등록 jsp ${currentUser} hong/1111 -> Member
+                // 방법 1) session.setAttribute("currentUser", memberVO);
+                // 방법 2) @SessionAttribute를 class명 위에 설정한다.
+                model.addAttribute("currentUser", memberVO);
+                return "redirect:/ ";
             }
-
-
         }
-
     }
 }
--- springboard/src/main/webapp/index.jsp
+++ springboard/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,8 +9,23 @@
 </head>
 <body>
 	환영합니다. 첫 페이지에요.
+	<br />
 	<a href="${pageContext.request.contextPath}/board">게시판</a>
-	<a href="${pageContext.request.contextPath}/board/new">글쓰기</a>
-	<a href="${pageContext.request.contextPath}/login">로그인</a>
+	<br />
+	<a href="${pageContext.request.contextPath}/board/new">새글쓰기</a>
+	<br />
+	<a href="${pageContext.request.contextPath}/signup">회원가입하기</a>
+	<br />
+
+	<c:if test="${empty currentUser}">
+		<a href="${pageContext.request.contextPath}/login">로그인하기</a>
+		<br />
+	</c:if>
+	<c:if test="${not empty currentUser}">
+	  	${currentUser.name}님 안녕하세요.<br>
+		<a href="${pageContext.request.contextPath}/logout">로그아웃</a>
+		<br />
+	</c:if>
+
 </body>
 </html>
